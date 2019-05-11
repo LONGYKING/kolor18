ba78eb979ecab7d4e3c4fafc39ff967abdb8db2e
LONGYKING <39104274+LONGYKING@users.noreply.github.com>
2019-05-11 20:38:33 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const mongoose   = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes', { useNewUrlParser : true}, (err) => {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/k18', { useNewUrlParser : true}, (err) => {
     if(err){
         console.log(err);
     }
