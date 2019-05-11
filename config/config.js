2dd5fa06216b5a6b5d72bf8622d229e12b71d531
LONGYKING <39104274+LONGYKING@users.noreply.github.com>
2019-05-11 21:48:41 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const mongoose   = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://okwarafrank:nHBOxulT54kxQvCE@cluster0-qd9wy.mongodb.net/test?retryWrites=true', { useNewUrlParser : true}, (err) => {
+mongoose.connect('mongodb+srv://okwarafrank:nHBOxulT54kxQvCE@cluster0-qd9wy.mongodb.net/test?retryWrites=true', { useNewUrlParser : true}, (err) => {
     if(err){
         console.log(err);
     }
