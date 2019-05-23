721cd89fc1898e079ea888d0490be14ad1276327
LONGYKING <39104274+LONGYKING@users.noreply.github.com>
2019-05-23 15:33:01 +0100

    Update blog.js

--- routes/blog.js
+++ routes/blog.js
@@ -1,81 +1,104 @@
 //import { Module } from 'module';
 var express = require('express');
-var _ = require('lodash');
-var auth = require('../middleware/auth.js');
-var groupAuth = require('../middleware/groupAuth.js');
-var user = require('../models/usermodel');
-var group = require('../models/groupmodel');
-var groupMember = require('../models/groupmember');
-var like = require('../models/likemodel');
-var share = require('../models/sharemodel');
-var post = require('../models/postmodel');
-var request = require('../models/requestmodel');
-var groupAdmin = require('../models/groupAdminmodel');
-var uploads = require('../helpers/uploads');
-var texts = require('../helpers/text');
-var feeds = require('../helpers/feedsFunc');
-var api = require('../helpers/apis/blogapi');
-const { ObjectID } = require('mongodb');
+var _       = require('lodash');
+const mailer   = require('nodemailer');
+var router  = express.Router();
+
+var {user}    = require('../models/usermodel');
+var {post}  = require('../models/postmodel');
+var {findByInterest}   = require('../models/activitymodel');
+var auth    = require('../middleware/auth');
+var alt     = require('../models/AlternateModel');
+var {GetFriendRequests} = require('../models/RelationModel');;
 
 var router = express.Router();
 
 //getting the blog page
 router.get('/blog',auth.authenticate, function(req, res, next) {
-    //getting the blog posts
-    var arg = "Blog";
-    post.findByObj(arg).then((posts) => {
-        Object.keys(posts).forEach(function(prop){
-            var blog_id = posts[prop]._id;
-            var owner = posts[prop].author;
-            //console.log(owner);
-            //getting the like for each blog post
-            like.getLikes(blog_id).then((likes) => {
-                Newuser.likes = likes.length;
-                Newuser.author = owner;
-                Newuser = req.user.toJSON();
-                Newuser.posts = posts;
-                res.render('blog', Newuser);
-            });
-           
-        });
-        //console.log(alls);
+
+    findByInterest(req.InteriorUser._id, "blog").then((Feeds) => {
         
-    });
+        Interior   = req.InteriorUser;
+        
+          res.render('blog',{
+            Interior,Feeds,
+          });
+        }).catch((e) => {
+          res.send(e);
+        });
 });
 
-//liking the posts in the blog page
-router.get('/likes/:id/:owner',auth.authenticate,function(req, res, next){
+// //liking the posts in the blog page
+// router.get('/likes/:id/:owner',auth.authenticate,function(req, res, next){
     
-    //getting the id of the post
-    var ids = req.params.id;
-    //getting the post owner
-    var owner = req.params.owner;
-    //getting the current user in the system
-    var user = req.user.firstname;
+//     //getting the id of the post
+//     var ids = req.params.id;
+//     //getting the post owner
+//     var owner = req.params.owner;
+//     //getting the current user in the system
+//     var user = req.user.firstname;
 
-    //adding the likes
-    //checking if the current has liked the post already
-    like.getLikesbyUser(user, ids).then((likes) => {
-        //checking if the user has like the post already
-        if (!likes) {
-            //getting the full details to be inserted
-            var likeObj = new like({ by: user, mainOwner: owner, mainObj: ids });
-            like.create(likeObj).then((getLikes) => {
-                console.log(likeObj);
-                if(getLikes){
-                    res.redirect('back');
-                }else{
-                    res.redirect('back');
-                }
-                //cb(err, getLikes);
-            });
-        }else{
-            res.redirect('back');
-        }
+//     //adding the likes
+//     //checking if the current has liked the post already
+//     like.getLikesbyUser(user, ids).then((likes) => {
+//         //checking if the user has like the post already
+//         if (!likes) {
+//             //getting the full details to be inserted
+//             var likeObj = new like({ by: user, mainOwner: owner, mainObj: ids });
+//             like.create(likeObj).then((getLikes) => {
+//                 console.log(likeObj);
+//                 if(getLikes){
+//                     res.redirect('back');
+//                 }else{
+//                     res.redirect('back');
+//                 }
+//                 //cb(err, getLikes);
+//             });
+//         }else{
+//             res.redirect('back');
+//         }
 
-    });
+//     });
 
-  // res.render('groups');
-});
+//   // res.render('groups');
+// });
+
+// //searching the blog post in the blog page
+// router.post('/search',auth.authenticate, function(req, res, next){
+//     var searchKey = req.body.blog_search;
+//     var blog = "Blog";
+
+//     //console.log(searchKey);
+//     if(searchKey == ""){
+//         return res.send("Enter A Search Key");
+//     }
+//     post.searchBlogPost(searchKey, blog).then((posts) => {
+//         Newuser.posts = posts;
+//         //console.log(posts);
+//         //Newuser = req.user.toJSON();
+
+//         res.render('blog_result', Newuser);
+//     });
+// });
+
+// //viewing more infromation about the blog post in the blog page
+// router.get('/more/:blog_id',auth.authenticate,function(req, res, next) {
+
+//     var id = req.params.blog_id;
+//     var blog = "Blog";
+
+//     //console.log(id);
+//     post.findPostByObjs(blog,id).then((blogs) => {
+//         post.searchBlogById(id,blog).then((post) => {
+//             console.log(post);
+//             Newuser = req.user.toJSON();
+//             Newuser.blogs = post;
+//             Newuser.blogPost = blogs;
+//             res.render('view_blog', Newuser);
+//         });
+//     });
+
+
+// });
 
 module.exports = router;
