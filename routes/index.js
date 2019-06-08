e007134b98230ca450b8a305e6a8e710c9d1dcb7
LONGYKING <39104274+LONGYKING@users.noreply.github.com>
2019-06-08 16:27:03 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var _       = require('lodash');
 const mailer   = require('nodemailer');
 var router  = express.Router();
-var {generateOTP}    = require('../helpers/help');
+
 var {user}    = require('../models/usermodel');
 var {post}  = require('../models/postmodel');
 var feeds   = require('../models/activitymodel');
@@ -10,7 +10,6 @@ var auth    = require('../middleware/auth');
 
 var alt     = require('../models/AlternateModel');
 var {GetFriendRequests} = require('../models/RelationModel');
-var Failed  = require('../languages/success');
 
 /* GET home page. */
 router.get('/@/:id', auth.authenticate,auth.finduser, function(req, res, next) {
@@ -76,9 +75,7 @@ router.get('/', auth.authenticate, function(req, res, next) {
     });
 });
 
-router.get('/test', function(req, res, next){
- res.sendFile('index.ejs');
-});
+
 
 router.get('/register', auth.active, function(req, res, next){
   res.render('register');
@@ -96,7 +93,6 @@ router.post('/register', function(req, res, next){
   }).then((token) => {
 
     req.session.x_auth = token;
-    req.session.code = generateOTP;
     res.header('x-auth', token).redirect('/auth');
   }).catch((e) => {
     return res.send(e);
@@ -105,32 +101,7 @@ router.post('/register', function(req, res, next){
 });
 
 router.get('/auth', auth.authenticate, function(req, res, next){
-  let transporter = mailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: 'kingsleyonyeneke@gmail.com',
-        pass: '09034128815'
-    }
-});
-let mailOptions = {
-    from: 'InstaTrend@gmail.com', // sender address
-    to: req.InteriorUser.email, // list of receivers
-    subject:  'VERIFICATION',
-    text: `YOUR VERIFICATION CODE => ${req.session.code}`, // plain text body
-    html: `<b>Hello ${req.InteriorUser.firstname} <br> your verification code => ${req.session.code}</b>` // html body
-};
-
-transporter.sendMail(mailOptions, (error, info) => {
-  var message = error;
-    if (error) {
-        console.log(error);
-        return res.send(error);
-    }
-    console.log('Message %s sent: %s', info.messageId, info.response);
-    res.render('verification',{message});
-    });
+  res.render('verification');
 });
 
 
@@ -144,7 +115,6 @@ router.post('/login', (req, res) => {
       res.header('x-auth', token).redirect('/');
     });
   }).catch((e) => {
-    var error  = Failed.ToLogin;
     res.status(400).redirect('/register', 400, {error});
   });
 });
