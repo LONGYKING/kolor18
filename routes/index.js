9fc6a29d2f00521343a89989cab1b1c0912ac5d6
LONGYKING <39104274+LONGYKING@users.noreply.github.com>
2019-06-08 16:07:27 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -84,34 +84,7 @@ router.get('/register', auth.active, function(req, res, next){
   res.render('register');
 });
 
-router.get('/auth', auth.authenticate, function(req, res, next){
-  let transporter = mailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: 'kingsleyonyeneke@gmail.com',
-        pass: '09034128815'
-    }
-});
-let mailOptions = {
-    from: 'InstaTrend@gmail.com', // sender address
-    to: req.InteriorUser.email, // list of receivers
-    subject:  'VERIFICATION',
-    text: `YOUR VERIFICATION CODE => ${req.session.code}`, // plain text body
-    html: `<b>Hello ${req.InteriorUser.firstname} <br> your verification code => ${req.session.code}</b>` // html body
-};
 
-transporter.sendMail(mailOptions, (error, info) => {
-  var message = error;
-    if (error) {
-        console.log(error);
-        return res.send(error);
-    }
-    console.log('Message %s sent: %s', info.messageId, info.response);
-    res.render('verification',{message});
-    });
-});
 
 router.post('/register', function(req, res, next){
   req.body.avatar = req.body.Gender == 'male' ? 'male.png' : 'female.png';
