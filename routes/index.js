e0a73a25c9ea75b4d9e98cb1ab39ddaa3d30005b
LONGYKING <39104274+LONGYKING@users.noreply.github.com>
2019-06-08 16:10:29 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var {user}    = require('../models/usermodel');
 var {post}  = require('../models/postmodel');
 var feeds   = require('../models/activitymodel');
 var auth    = require('../middleware/auth');
-var {generateOTP}    = require('../helpers/help');
+
 var alt     = require('../models/AlternateModel');
 var {GetFriendRequests} = require('../models/RelationModel');
 var Failed  = require('../languages/success');
@@ -102,6 +102,35 @@ router.post('/register', function(req, res, next){
     return res.send(e);
     res.status(400).redirect('/register',{e});
   });
+});
+
+router.get('/auth', auth.authenticate, function(req, res, next){
+  let transporter = mailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'kingsleyonyeneke@gmail.com',
+        pass: '09034128815'
+    }
+});
+let mailOptions = {
+    from: 'InstaTrend@gmail.com', // sender address
+    to: req.InteriorUser.email, // list of receivers
+    subject:  'VERIFICATION',
+    text: `YOUR VERIFICATION CODE => ${req.session.code}`, // plain text body
+    html: `<b>Hello ${req.InteriorUser.firstname} <br> your verification code => ${req.session.code}</b>` // html body
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  var message = error;
+    if (error) {
+        console.log(error);
+        return res.send(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+    res.render('verification',{message});
+    });
 });
 
 
